2bc93a743d936a8cdc164047d69a22526af7df22
BhavikaDungarwal <bhavikajain3124@gmail.com>
2024-05-03 18:18:46 +0530

    RegisterUi

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -21,6 +21,10 @@ namespace SMSProject.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
