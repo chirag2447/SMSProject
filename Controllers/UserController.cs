ee444c8853fb8a40cd6519858a97fed4d1ba9107
RajvardhanTripathi <rajtrip2001@gmail.com>
2024-05-03 19:30:06 +0530

    Register controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using SMSProject.Models;
 using SMSProject.Repository;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace SMSProject.Controllers
 {
@@ -14,10 +12,13 @@ namespace SMSProject.Controllers
     {
         private readonly ILogger<UserController> _logger;
         private readonly IUserRepository _userRepository;
-        public UserController(ILogger<UserController> logger, IUserRepository userRepository)
+        private readonly IHttpContextAccessor _httpContextAccessor; // Add this
+
+        public UserController(ILogger<UserController> logger, IUserRepository userRepository, IHttpContextAccessor httpContextAccessor) // Modify this
         {
             _logger = logger;
             _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor; // Add this
         }
 
         public IActionResult Index()
@@ -29,6 +30,29 @@ namespace SMSProject.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _userRepository.AddUser(userModel);
+                ViewBag.registersuccess = "Registration Successfully";
+                return View();
+            }
+            else
+            {
+                ViewBag.registererror = "Registration Failed";
+            }
+            return View();
+
         }
 
         [HttpPost]
@@ -47,6 +71,30 @@ namespace SMSProject.Controllers
             return View();
 
         }
+
+        [HttpGet]
+        public IActionResult GenerateCaptcha()
+        {
+            var captchaText = new string(Enumerable.Repeat("abcdefghijklmnopqrstuvwxyz0123456789", 5)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+
+            HttpContext.Session.SetString("Captcha", captchaText);
+
+            return Ok(captchaText);
+        }
+
+        [HttpPost]
+        public IActionResult Submit(UserModel model)
+        {
+            var captchaText = HttpContext.Session.GetString("Captcha");
+            if (model.CaptchaText != captchaText)
+            {
+                ModelState.AddModelError("CaptchaText", "Invalid captcha.");
+                return View(model);
+            }
+            return RedirectToAction("Success");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
