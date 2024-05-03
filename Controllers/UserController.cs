160fe1d94dcd8cfe5bca7ebdd776207ff5a21c10
RajvardhanTripathi <rajtrip2001@gmail.com>
2024-05-03 15:27:33 +0530

    logindone

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,21 +5,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SMSProject.Models;
+using SMSProject.Repository;
 
 namespace SMSProject.Controllers
 {
     public class UserController : Controller
     {
         private readonly ILogger<UserController> _logger;
-
-        public UserController(ILogger<UserController> logger)
+        private readonly IUserRepository _userRepository;
+        public UserController(ILogger<UserController> logger, IUserRepository userRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel usermodel)
+        {
+
+            if (_userRepository.Login(usermodel))
+            {
+                ViewBag.loginsuccess = "Login Successfully";
+                return View();
+            }
+            else
+            {
+                ViewBag.loginerror = "Not a valid Credential";
+            }
+            return View();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
